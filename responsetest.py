550d4e05d34134782174336abe53fdfe812f3ba6
Nelis Volschenk <nelisvolschenk@gmail.com>
2021-05-05 12:18:45 +0200

    updated to new velib_python

--- responsetest.py
+++ responsetest.py
@@ -1,11 +1,20 @@
 
 import dbus
+from dbus.mainloop.glib import DBusGMainLoop
+import glib
 from ext.velib_python.vedbus import VeDbusItemImport
 
+def print_value():
+    print "Value changed"
+
+DBusGMainLoop(set_as_default=True)
 SysBus = dbus.SystemBus()
 proxy = VeDbusItemImport(
     bus=SysBus,
     serviceName="com.victronenergy.system",
     path="/Ac/Consumption/L1/Power",
-    eventCallback=None,
-    createsignal=True).get_value()
+    eventCallback=print_value,
+    createsignal=True)
+
+mainloop = glib.Mainloop()
+mainloop.run()
