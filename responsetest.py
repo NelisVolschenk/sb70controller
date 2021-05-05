394b7824dfedf465c4aa5c1ae4f84fbf230384f7
Nelis Volschenk <nelisvolschenk@gmail.com>
2021-05-05 15:19:48 +0200

    testing

--- responsetest.py
+++ responsetest.py
@@ -2,12 +2,13 @@
 import dbus
 from dbus.mainloop.glib import DBusGMainLoop
 import glib
+from datetime import datetime
 from ext.velib_python.vedbus import VeDbusItemImport
 from ext.velib_python.ve_utils import unwrap_dbus_value
 
 def print_value(name, path, changes):
     value = changes['Value']
-    print "Value changed to %s" % value
+    print datetime.now(), " - Value changed to ", value
 
 DBusGMainLoop(set_as_default=True)
 SysBus = dbus.SystemBus()
